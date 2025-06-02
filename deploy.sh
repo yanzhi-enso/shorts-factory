7d06c26fc3c34a73ae6f38f54277a96e59eb6569
Yanzhi <yanzhi@enso-ai.com>
2025-06-01 23:02:58 -0700

    init

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+gcloud run deploy shorts-factory \
+    --source . \
+    --region us-west1 \
+    --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com"
