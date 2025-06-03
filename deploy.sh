fe5b8d9f91534b2e6a11cca359a0cbfe4943fada
Yanzhi <yanzhi@enso-ai.com>
2025-06-03 01:49:17 -0700

    add more support for gcp/gcs access

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 gcloud run deploy shorts-factory \
     --source . \
     --region us-west1 \
+    --cpu 2 \
+    --memory 8Gi \
+    --timeout 180 \
+    --service-account="shorts-factory-sa@pure-lantern-394915.iam.gserviceaccount.com" \
     --build-service-account "projects/pure-lantern-394915/serviceAccounts/cloud-build@pure-lantern-394915.iam.gserviceaccount.com"
