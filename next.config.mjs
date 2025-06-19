04486a23d498d68f1b9cfbd9e12373fa054803e6
yanzhi <yanzhi@enso-ai.com>
2025-06-19 01:56:46 -0700

    tmp add kling cdn in next remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/shorts-scenes/**',
-      },
-    ],
-  },
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                port: '',
+                pathname: '/shorts-scenes/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.klingai.com',
+                port: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
