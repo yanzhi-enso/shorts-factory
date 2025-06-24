c6300e1ff5e1e91d59cb4cb0266e04dbe7eeabef
Yanzhi <yanzhi@enso-ai.com>
2025-06-24 11:18:15 -0700

    format code

--- src/app/components/remake/ToolBoxBlock.js
+++ src/app/components/remake/ToolBoxBlock.js
@@ -3,44 +3,44 @@ import Image from 'next/image';
 import { FaPlus } from 'react-icons/fa';
 import styles from './ToolBoxBlock.module.css';
 
-const ToolBoxBlock = ({ 
-  src, 
-  onClick 
+const ToolBoxBlock = ({
+    src,
+    onClick
 }) => {
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    }
-  };
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        }
+    };
 
-  const renderContent = () => {
-    if (!src) {
-      return (
-        <div className={styles.emptyState}>
-          <FaPlus className={styles.plusIcon} />
-        </div>
-      );
-    }
+    const renderContent = () => {
+        if (!src) {
+            return (
+                <div className={styles.emptyState}>
+                    <FaPlus className={styles.plusIcon} />
+                </div>
+            );
+        }
+
+        return (
+            <Image
+                src={src}
+                alt="Tool box item"
+                width={100}
+                height={100}
+                className={styles.image}
+            />
+        );
+    };
 
     return (
-      <Image
-        src={src}
-        alt="Tool box item"
-        width={100}
-        height={100}
-        className={styles.image}
-      />
+        <div
+            className={`${styles.toolBoxBlock} ${!src ? styles.empty : ''}`}
+            onClick={handleClick}
+        >
+            {renderContent()}
+        </div>
     );
-  };
-
-  return (
-    <div 
-      className={`${styles.toolBoxBlock} ${!src ? styles.empty : ''}`} 
-      onClick={handleClick}
-    >
-      {renderContent()}
-    </div>
-  );
 };
 
 export default ToolBoxBlock;
