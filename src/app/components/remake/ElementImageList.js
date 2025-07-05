22a61b089d3a17479a84fcf6cf1f4a9951a6a949
Yanzhi <yanzhi@enso-ai.com>
2025-07-05 13:59:33 -0700

    move elementImageModal to remake folder

--- src/app/components/remake/ElementImageList.js
+++ src/app/components/remake/ElementImageList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { FaPlus } from 'react-icons/fa';
 import { useProjectManager } from 'hocs/ProjectManager';
-import ElementImageModal from 'app/components/common/ElementImageModal';
+import ElementImageModal from 'app/components/remake/ElementImageModal';
 import styles from './ElementImageList.module.css';
 
 const ElementImageBlock = ({ src, onClick }) => {
